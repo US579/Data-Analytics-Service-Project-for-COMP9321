1a291a3b1f69b2617b37fc0ad0b84d6e1f9482fd
us579 <wzus579@gmail.com>
2018-10-17 14:49:54 +1100

    update the login function

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,21 +1,46 @@
 var submit = document.getElementById("button");
 
+
 submit.onclick = function(){
-    /*var url="http://127.0.0.1:5000/worldbanks"
+
+    var  username =document.getElementById('username').value
+    var  password =document.getElementById('password').value
+    var url="http://127.0.0.1:5000/login"
     $.ajax({
         url: url,
         type: 'GET',
-        dataType: 'JSON',
-        success: function(data){
-            alert("ss");
-        console.log(data);
+        data:{
+            'username':username,
+            'password':password,
         },
-        error: function(data){
-            alert("fff")
+        dataType: 'JSONP',
+        crossDomain:true,
+
+        success: function(XMLHttpRequest, textStatus,data,xhr){
+            var obj = JSON.parse(data.responseJSON);
+            if (obj.message == 'authorization has been refused for those credentials.')
+            {
+                alert('username and password are not correct');
+                return
+            }
+            else{
+                window.location.href = './main.html';
+            }
+
+            //alert('success');
+            console.log(data.responseJSON);
+            //window.location.href = './main.html';
+            
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown){
+            
+            alert(XMLHttpRequest.status);
+            alert(XMLHttpRequest.readyState);
+            alert(textStatus);
         }
         
-    }
-    )*/
-    window.location.href = 'main.html';
+    })
+    
   
 }
+
